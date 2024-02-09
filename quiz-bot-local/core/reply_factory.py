0bd3649e4085a9603999693abc50dc8dcb4f20e3
ADUMAPATHI <adumapathi101@gmail.com>
2024-02-09 22:21:21 +0530

    "First commit"

---
+++ quiz-bot-local/core/reply_factory.py
@@ -0,0 +1,58 @@
+
+from .constants import BOT_WELCOME_MESSAGE, PYTHON_QUESTION_LIST
+
+
+def generate_bot_responses(message, session):
+    bot_responses = []
+
+    current_question_id = session.get("current_question_id")
+    if not current_question_id:
+        bot_responses.append(BOT_WELCOME_MESSAGE)
+
+    success, error = record_current_answer(message, current_question_id, session)
+
+    if not success:
+        return [error]
+
+    next_question, next_question_id = get_next_question(current_question_id)
+
+    if next_question:
+        bot_responses.append(next_question)
+    else:
+        final_response = generate_final_response(session)
+        bot_responses.append(final_response)
+
+    session["current_question_id"] = next_question_id
+    session.save()
+
+    return bot_responses
+
+
+def record_current_answer(answer, current_question_id, session):
+    '''
+    Validates and stores the answer for the current question to django session.
+    '''
+    session[PYTHON_QUESTION_LIST[current_question_id]["question_text"]] = answer 
+    session.save()
+    if PYTHON_QUESTION_LIST[current_question_id]['answer'] == answer:
+        return True, ""
+    else:
+        return False, " Answer is wrong "
+
+
+def get_next_question(current_question_id):
+    '''
+    Fetches the next question from the PYTHON_QUESTION_LIST based on the current_question_id.
+    '''
+    return PYTHON_QUESTION_LIST[current_question_id+1]["question_text"],[current_question_id+1] 
+
+
+def generate_final_response(session):
+    '''
+    Creates a final result message including a score based on the answers
+    by the user for questions in the PYTHON_QUESTION_LIST.
+    '''
+    dic = {}
+    for i,qtn in enumerate(PYTHON_QUESTION_LIST):
+        dic[qtn["question_text"]] = session[qtn["answer"]]
+    return dic
